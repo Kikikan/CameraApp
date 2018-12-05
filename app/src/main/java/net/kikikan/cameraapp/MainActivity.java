2cf9fdee586d00ba014342d3767a8e73e32daee7
Kikikan <kikikkan@freemail.hu>
2018-12-05 18:58:39 +0100

    basic UI

--- app/src/main/java/net/kikikan/cameraapp/MainActivity.java
+++ app/src/main/java/net/kikikan/cameraapp/MainActivity.java
@@ -1,13 +1,30 @@
 package net.kikikan.cameraapp;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public SeekBar sb;
+    public EditText delayET;
+    TextureView cameraView;
+
+    private final int CAMERA_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
         setContentView(R.layout.activity_main);
+        sb = findViewById(R.id.delaySeekBar);
+        delayET = findViewById(R.id.delayEditText);
+        cameraView = findViewById(R.id.cameraView);
+
+        sb.setOnSeekBarChangeListener(new SeekBarManager(this));
     }
 }
---
+++ app/src/main/java/net/kikikan/cameraapp/SeekBarManager.java
@@ -0,0 +1,48 @@
+package net.kikikan.cameraapp;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.SeekBar;
+
+public class SeekBarManager implements SeekBar.OnSeekBarChangeListener{
+
+    private MainActivity mainActivity;
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if (b)
+            mainActivity.delayET.setText(String.valueOf(i));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    public SeekBarManager(MainActivity ac) {
+        mainActivity = ac;
+
+        mainActivity.delayET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!mainActivity.delayET.getText().toString().equals(""))
+                    mainActivity.sb.setProgress(Integer.parseInt(mainActivity.delayET.getText().toString()));
+            }
+        });
+    }
+}
