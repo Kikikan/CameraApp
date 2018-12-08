3a308841fd2344d7bc0d553647765bd75f62bebe
Vencel7881 <vincentzcouch@gmail.com>
2018-12-08 16:10:19 +0100

    imageview

--- app/src/main/java/net/kikikan/cameraapp/MainActivity.java
+++ app/src/main/java/net/kikikan/cameraapp/MainActivity.java
@@ -1,11 +1,19 @@
 package net.kikikan.cameraapp;
 
 import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     public SeekBar sb;
     public TextView delayTW;
-    TextureView cameraView;
+    ImageView cameraView;
+    Button takeButton;
 
     private final int CAMERA_REQUEST_CODE = 100;
 
@@ -25,7 +34,31 @@ public class MainActivity extends AppCompatActivity {
         sb = findViewById(R.id.delaySeekBar);
         delayTW = findViewById(R.id.delaytextView);
         cameraView = findViewById(R.id.cameraView);
+        takeButton = (Button) findViewById(R.id.takeButton);
 
         sb.setOnSeekBarChangeListener(new SeekBarManager(this));
+
+        takeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == CAMERA_REQUEST_CODE)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            cameraView.setImageBitmap(photo);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        return true;
     }
 }
