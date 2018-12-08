0c22934c660cf5dbf5cffde5d847865cbcf1d3e2
Kikikan <kikikkan@freemail.hu>
2018-12-08 16:32:47 +0100

    jó már?

--- app/src/main/java/net/kikikan/cameraapp/MainActivity.java
+++ app/src/main/java/net/kikikan/cameraapp/MainActivity.java
@@ -65,7 +65,14 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
-                cameraView.setImageBitmap(imageBitmap);
+                Matrix matrix = new Matrix();
+
+                matrix.postRotate(90);
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth(), imageBitmap.getHeight(), true);
+
+                Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+                cameraView.setImageBitmap(rotatedBitmap);
             }
         }
     }
